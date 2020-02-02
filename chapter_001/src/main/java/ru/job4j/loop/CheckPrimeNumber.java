9301efb54c88261714dc9fd1825f0c3866f33911
Andrii Ryzhyi <andrii.nrj@gmail.com>
2020-02-02 21:44:03 +0200

    Простое число.[#229914]

---
+++ chapter_001/src/main/java/ru/job4j/loop/CheckPrimeNumber.java
@@ -0,0 +1,16 @@
+package ru.job4j.loop;
+
+public class CheckPrimeNumber {
+    public static boolean check(int finish) {
+        boolean prime = true;
+        if (finish == 1)
+            prime = false;
+        for (int i = 2; i <= finish / 2; i++) {
+            if (finish % i == 0) {
+                prime = false;
+                break;
+            }
+        }
+        return prime;
+    }
+}
