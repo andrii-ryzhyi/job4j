507fd9783b8a2b309d32d2053e2379b6d0ca7051
Andrii Ryzhyi <Andrii.Ryzhyi@infopulse.com>
2020-02-03 16:46:45 +0200

    JavaDoc. Code convention[#229919]

--- chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
+++ chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
@@ -1,5 +1,11 @@
 package ru.job4j.calculator;
 
+/**
+ * Class Calculate for arithmetic operations +, -, /, *.
+ * @author Andrii.Ryzhyi
+ * @since 03.02.2020
+ * @version 1.0
+ */
 public class Calculator {
     public static void add(double first, double second) {
         double result =  first + second;
@@ -17,6 +23,11 @@ public class Calculator {
         double result = first - second;
         System.out.println(first + "-" + second + "=" + result);
     }
+
+    /**
+     * Main
+     * @param args - args
+     */
     public static void main(String[] args) {
         add(1, 1);
         add(2, 2);
