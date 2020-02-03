37ffc137f42f3a298fcf5ad6ec532a817a6bb89e
Andrii Ryzhyi <Andrii.Ryzhyi@infopulse.com>
2020-02-03 17:45:10 +0200

    Заполнение массива.[#229863]

--- chapter_001/src/main/java/ru/job4j/array/ArrayDefinition.java
+++ chapter_001/src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -5,5 +5,14 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surnames = new String[100500];
         float[] prices = new float[40];
+
+        String[] names = new String[4];
+        names[0] = "Today";
+        names[1] = "is";
+        names[2] = "Monday";
+        names[3] = "!";
+
+        for (int i = 0; i < names.length; i++)
+            System.out.print(names[i] + " ");
     }
 }
