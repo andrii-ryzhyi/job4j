f93d9e32c962a179ff89b503e01306b4ce54ad38
Andrii.Ryzhyi <andrii.nrj@gmail.com>
2020-02-10 12:40:30 +0200

    Вызов метода с аргументами.[#230013]

--- chapter_002/src/main/java/ru/job4j/oop/Student.java
+++ chapter_002/src/main/java/ru/job4j/oop/Student.java
@@ -7,6 +7,9 @@ public class Student {
     public void song() {
         System.out.println("I believe I can fly");
     }
+    public void music(String lyrics) {
+        System.out.println("I can sign a song : " + lyrics);
+    }
     public static void main(String[] args) {
         Student petya = new Student();
         petya.music();
@@ -15,5 +18,7 @@ public class Student {
         petya.song();
         petya.song();
         petya.song();
+        String song = "I believe, I can fly";
+        petya.music(song);
     }
 }
